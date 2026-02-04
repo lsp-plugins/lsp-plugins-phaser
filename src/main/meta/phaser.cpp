af2e01dd02e670f2b1b323a97dc0255d58e780a1
Vladimir Sadovnikov <sadko4u@gmail.com>
2026-02-04 23:45:10 +0300

    Using new mechanism for registering plugin metadata

--- src/main/meta/phaser.cpp
+++ src/main/meta/phaser.cpp
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2025 Linux Studio Plugins Project <https://lsp-plug.in/>
- *           (C) 2025 Vladimir Sadovnikov <sadko4u@gmail.com>
+ * Copyright (C) 2026 Linux Studio Plugins Project <https://lsp-plug.in/>
+ *           (C) 2026 Vladimir Sadovnikov <sadko4u@gmail.com>
  *
  * This file is part of lsp-plugins-phaser
  * Created on: 10 мар 2025 г.
@@ -20,6 +20,7 @@
  */
 
 #include <lsp-plug.in/plug-fw/meta/ports.h>
+#include <lsp-plug.in/plug-fw/meta/registry.h>
 #include <lsp-plug.in/shared/meta/developers.h>
 #include <private/meta/phaser.h>
 
@@ -298,6 +299,7 @@ namespace lsp
             mono_plugin_port_groups,
             &phaser_bundle
         };
+        LSP_REGISTER_METADATA(phaser_mono);
 
         const plugin_t phaser_stereo =
         {
@@ -328,6 +330,8 @@ namespace lsp
             stereo_plugin_port_groups,
             &phaser_bundle
         };
+        LSP_REGISTER_METADATA(phaser_stereo);
+
     } /* namespace meta */
 } /* namespace lsp */
 
